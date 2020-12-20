0715b35ea7b7b9a98eb8df0d10a410f5ed8508ac
Nigel Lee <lee.nigel@hotmail.com>
2020-12-20 21:23:17 +0800

    modified directions

--- src/service/AdjacencyList.ts
+++ src/service/AdjacencyList.ts
@@ -5,7 +5,16 @@ export interface IAdjacencyList {
 }
 
 export default function AdjacencyList(rows: number, cols: number) {
-  const DIRECTIONS = [-1, 0, 1];
+  const DIRECTIONS = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, 1],
+    [0, -1],
+    [1, 1],
+    [1, 0],
+    [1, -1],
+  ];
 
   const grid = chunk(range(0, rows * cols, 1), cols);
 
@@ -13,23 +22,18 @@ export default function AdjacencyList(rows: number, cols: number) {
   grid.forEach((row, i) => {
     row.forEach((entry, j) => {
       let curr: number[] = [];
-      DIRECTIONS.forEach((rowDir) => {
-        DIRECTIONS.forEach((colDir) => {
-          if (rowDir === 0 && colDir === 0) {
-            return;
-          }
-          const nextRow = i + rowDir;
-          const nextCol = j + colDir;
-          if (
-            nextRow < 0 ||
-            nextRow >= grid.length ||
-            nextCol < 0 ||
-            nextCol >= grid[0].length
-          ) {
-            return;
-          }
-          curr.push(grid[nextRow][nextCol]);
-        });
+      DIRECTIONS.forEach((dir) => {
+        const nextRow = i + dir[0];
+        const nextCol = j + dir[1];
+        if (
+          nextRow < 0 ||
+          nextRow >= grid.length ||
+          nextCol < 0 ||
+          nextCol >= grid[0].length
+        ) {
+          return;
+        }
+        curr.push(grid[nextRow][nextCol]);
       });
 
       adjList[entry] = curr;
