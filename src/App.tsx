2055299af5a19fd9a6e35dd3b97ed5792af3eef7
Nigel Lee <lee.nigel@hotmail.com>
2020-12-25 22:55:26 +0800

    style(src): Fix App.tsx formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import {
-  NodeCollapseOutlined,
-  NodeExpandOutlined,
-  StopOutlined,
+	NodeCollapseOutlined,
+	NodeExpandOutlined,
+	StopOutlined,
 } from "@ant-design/icons";
 import { Button, Menu, Tooltip, Typography } from "antd";
 import MenuItem from "antd/lib/menu/MenuItem";
@@ -16,81 +16,81 @@ import "./App.css";
 const { Title } = Typography;
 
 export enum DrawTools {
-  DrawObstacle,
-  DrawEndNode,
-  DrawStartNode,
+	DrawObstacle,
+	DrawEndNode,
+	DrawStartNode,
 }
 
 export function App() {
-  const [keyPressed, setKeyPressed] = useState<string | null>();
+	const [keyPressed, setKeyPressed] = useState<string | null>();
 
-  const [drawTool, setDrawTool] = useState<DrawTools | null>();
+	const [drawTool, setDrawTool] = useState<DrawTools | null>();
 
-  useEffect(() => {
-    const subSink = new SubSink();
+	useEffect(() => {
+		const subSink = new SubSink();
 
-    subSink.sink = keyDown$.subscribe(({ key }) => {
-      setKeyPressed(key);
-    });
+		subSink.sink = keyDown$.subscribe(({ key }) => {
+			setKeyPressed(key);
+		});
 
-    subSink.sink = keyUp$.subscribe(() => {
-      setKeyPressed(null);
-    });
+		subSink.sink = keyUp$.subscribe(() => {
+			setKeyPressed(null);
+		});
 
-    return () => {
-      subSink.unsubscribe();
-    };
-  }, []);
+		return () => {
+			subSink.unsubscribe();
+		};
+	}, []);
 
-  const handleDrawToolClick = (tool: DrawTools) => {
-    if (drawTool === tool) {
-      setDrawTool(null);
-    } else {
-      setDrawTool(tool);
-    }
-  };
+	const handleDrawToolClick = (tool: DrawTools) => {
+		if (drawTool === tool) {
+			setDrawTool(null);
+		} else {
+			setDrawTool(tool);
+		}
+	};
 
-  return (
-    <div>
-      <Menu
-        style={{ height: "100vh", width: "300px" }}
-        mode="vertical"
-        expandIcon={null}
-      >
-        <MenuItem disabled>
-          <Title level={3}>Pathfinding</Title>
-        </MenuItem>
-        <div
-          style={{ width: "100%", display: "flex", justifyContent: "center" }}
-        >
-          <Tooltip title="Draw Starting Node">
-            <Button
-              type={
-                drawTool === DrawTools.DrawStartNode ? "primary" : "default"
-              }
-              onClick={() => handleDrawToolClick(DrawTools.DrawStartNode)}
-            >
-              <NodeExpandOutlined />
-            </Button>
-          </Tooltip>
-          <Tooltip title="Draw Ending Node">
-            <Button
-              type={drawTool === DrawTools.DrawEndNode ? "primary" : "default"}
-              onClick={() => handleDrawToolClick(DrawTools.DrawEndNode)}
-            >
-              <NodeCollapseOutlined />
-            </Button>
-          </Tooltip>
-          <Tooltip title="Draw Obstacles">
-            <Button
-              type={drawTool === DrawTools.DrawObstacle ? "primary" : "default"}
-              onClick={() => handleDrawToolClick(DrawTools.DrawObstacle)}
-            >
-              <StopOutlined />
-            </Button>
-          </Tooltip>
-        </div>
-        {/* <Tag color={keyPressed === "o" ? "processing" : "default"}>
+	return (
+		<div>
+			<Menu
+				style={{ height: "100vh", width: "300px" }}
+				mode="vertical"
+				expandIcon={null}
+			>
+				<MenuItem disabled>
+					<Title level={3}>Pathfinding</Title>
+				</MenuItem>
+				<div
+					style={{ width: "100%", display: "flex", justifyContent: "center" }}
+				>
+					<Tooltip title="Draw Starting Node">
+						<Button
+							type={
+								drawTool === DrawTools.DrawStartNode ? "primary" : "default"
+							}
+							onClick={() => handleDrawToolClick(DrawTools.DrawStartNode)}
+						>
+							<NodeExpandOutlined />
+						</Button>
+					</Tooltip>
+					<Tooltip title="Draw Ending Node">
+						<Button
+							type={drawTool === DrawTools.DrawEndNode ? "primary" : "default"}
+							onClick={() => handleDrawToolClick(DrawTools.DrawEndNode)}
+						>
+							<NodeCollapseOutlined />
+						</Button>
+					</Tooltip>
+					<Tooltip title="Draw Obstacles">
+						<Button
+							type={drawTool === DrawTools.DrawObstacle ? "primary" : "default"}
+							onClick={() => handleDrawToolClick(DrawTools.DrawObstacle)}
+						>
+							<StopOutlined />
+						</Button>
+					</Tooltip>
+				</div>
+				{/* <Tag color={keyPressed === "o" ? "processing" : "default"}>
 						Draw Obstacle
 					</Tag>
 					<Tag color={keyPressed === "s" ? "processing" : "default"}>
@@ -99,10 +99,10 @@ export function App() {
 					<Tag color={keyPressed === "e" ? "processing" : "default"}>
 						Draw End
 					</Tag> */}
-      </Menu>
-      <NumberGrid />
-    </div>
-  );
+			</Menu>
+			<NumberGrid />
+		</div>
+	);
 }
 
 export default App;
