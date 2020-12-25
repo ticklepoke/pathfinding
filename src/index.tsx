c0eb0318ab4536724170848a7e534eb5ec356c4e
Nigel Lee <lee.nigel@hotmail.com>
2020-12-25 12:04:45 +0800

    Revert "ci(gh-actions): Fix typo for title"
    This reverts commit e812631aa826137126d3b6c1bcc8bbbf4454681e.

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import { initStore } from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const store = initStore();
 ReactDOM.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>,
-
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
